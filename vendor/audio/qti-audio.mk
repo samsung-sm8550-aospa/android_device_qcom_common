ffae0c84a20b1c41f27b38f017e4b39b14bfb8de
Bruno Martins <bgcngm@gmail.com>
2024-08-13 23:41:05 +0800

    vendor: audio: Skip building agm test binaries
    Change-Id: I6e0f4f37b4ae465700dfd4d20e54858a8241dcf4

--- vendor/audio/qti-audio.mk
+++ vendor/audio/qti-audio.mk
@@ -69,9 +69,6 @@ PRODUCT_PACKAGES += \
 
 ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
 PRODUCT_PACKAGES += \
-    agmcap \
-    agmcompressplay \
-    agmplay \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
